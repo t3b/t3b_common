8d0d63d3a8b5f72bfc3b04f5e995b8021d50b220
Anno v. Heimburg <avonheimburg@agenturwerft.de>
2013-02-24 12:12:48 +0100

    correct namespaces

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,4 @@ if (!defined('TYPO3_MODE')) {
 // Register pre-render hook for pagerenderer
 /** @var $_EXTKEY string */
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Hook/PreRender.php:\T3b\T3bCommon\Hook\PreRender->preRender';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . "Classes/Hook/PreRender.php:T3b\\T3bCommon\\Hook\\PreRender->preRender";
